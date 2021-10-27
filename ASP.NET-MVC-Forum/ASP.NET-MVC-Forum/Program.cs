f4088047f8ef050b3ea2aaa3902940075af57239
donchodonev <dummyacc9292@gmail.com>
2021-10-27 10:42:44 +0300

    added auto-migration on app startup for both production and development db

--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Program.cs
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Program.cs
@@ -1,13 +1,24 @@
 namespace ASP.NET_MVC_Forum
 {
+    using ASP.NET_MVC_Forum.Data;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
