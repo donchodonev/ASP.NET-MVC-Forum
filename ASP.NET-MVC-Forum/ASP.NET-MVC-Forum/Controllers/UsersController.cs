c565313e72e6a22f548ccbae78fde9264cf8d769
donchodonev <dummyacc9292@gmail.com>
2021-11-04 10:04:21 +0200

    implemented user profile picture upload with file extension validation and user feedback when uploaded file extension doesn't match

--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Controllers/UsersController.cs
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Controllers/UsersController.cs
@@ -5,7 +5,9 @@
     using ASP.NET_MVC_Forum.Services.User;
     using AutoMapper;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -17,7 +19,7 @@
         private readonly IUserService userService;
         private readonly IMapper mapper;
 
-        public UsersController(IPostService postService,IUserService userService, IMapper mapper)
+        public UsersController(IPostService postService, IUserService userService, IMapper mapper)
         {
             this.postService = postService;
             this.userService = userService;
@@ -31,6 +33,39 @@
             var vm = mapper.ProjectTo<PostPreviewViewModel>(await postService.GetByUserIdAsync(userId, withUserIncluded: true, withIdentityUserIncluded: true));
 
             return View("_PostsPreviewPartial", vm.ToList());
+        }
+
+        public async Task<IActionResult> UploadAvatar(IFormFile file)
+        {
+            string[] allowedFileExtensions = new string[5] { ".jpg", ".jpeg", ".png", ".webp",".bmp" };
+
+            string fileExtension = null;
+
+            foreach (var currentFileExtension in allowedFileExtensions)
+            {
+                if (file.FileName.EndsWith(currentFileExtension))
+                {
+                    fileExtension = currentFileExtension;
+                    break;
+                }
+            }
+
+            if (fileExtension == null)
+            {
+                TempData["Message"] = $"The allowed image file formats are {string.Join(' ',allowedFileExtensions)}";
+                return LocalRedirect("/Identity/Account/Manage#message");
+            }
+
+            var fileName = $"{this.User.Identity.Name}-avatar.jpg";
+            var filePath = @"C:\ASP.NET Forum Storage\";
+            var fullPath = Path.Combine(filePath, fileName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
