300716a61a09240bc218893cb486dc8f2aae7df5
donchodonev <dummyacc9292@gmail.com>
2021-11-04 12:04:02 +0200

    back go original idea, will use guid to transform image name and save that as url to user avatar = no way to know which user's image is that and mystifying it is done only once. security issue also addressed as randomly just querying user images won't work unless you already know the image url. Image for-eaching also impossible in this implementation of viewing the images

--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Controllers/UsersController.cs
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Controllers/UsersController.cs
@@ -21,12 +21,14 @@
         private readonly IPostService postService;
         private readonly IUserService userService;
         private readonly IMapper mapper;
+        private readonly IConfiguration configuration;
 
         public UsersController(IPostService postService, IUserService userService, IMapper mapper, IConfiguration configuration)
         {
             this.postService = postService;
             this.userService = userService;
             this.mapper = mapper;
+            this.configuration = configuration;
         }
 
         public async Task<IActionResult> UserPosts()
@@ -35,6 +37,74 @@
             var vm = mapper.ProjectTo<PostPreviewViewModel>(await postService.GetByUserIdAsync(userId, withUserIncluded: true, withIdentityUserIncluded: true));
 
             return View("_PostsPreviewPartial", vm.ToList());
+        }
+
+        public async Task<IActionResult> UploadAvatar(IFormFile file)
+        {
+            string[] allowedFileExtensions = new string[5] { ".jpg", ".jpeg", ".png", ".webp", ".bmp" };
+
+            string fileExtension = null;
+
+            foreach (var currentFileExtension in allowedFileExtensions)
+            {
+                if (file.FileName.EndsWith(currentFileExtension))
+                {
+                    fileExtension = currentFileExtension;
+                    break;
+                }
+            }
+
+            if (fileExtension == null)
+            {
+                TempData["Message"] = $"The allowed image file formats are {string.Join(' ', allowedFileExtensions)}";
+                return LocalRedirect("/Identity/Account/Manage#message");
+            }
+
+            var fileName = $"{this.User.Identity.Name}-avatar{fileExtension}";
+            var filePath = configuration.GetSection("FileUploadPath")["DefaultPath"];
+            var fullPath = Path.Combine(filePath, fileName);
+
+            string test = configuration.GetSection("FileUploadPath")["DefaultPath"];
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            TempData["Message"] = $"Your image has been successfully uploaded";
+
+            return LocalRedirect("/Identity/Account/Manage#message");
+        }
+
+        public async Task<IActionResult> GetAvatar([FromQuery]string username)
+        {
+            string[] allowedFileExtensions = new string[5] { ".jpg", ".jpeg", ".png", ".webp", ".bmp" };
+
+            var fileName = $"{this.User.Identity.Name}-avatar";
+            var directoryPath = configuration.GetSection("FileUploadPath")["DefaultPath"];
+            var fullPath = Path.Combine(directoryPath, fileName);
+
+            string expectedPathToAvatar = null;
+            string expectedFileExtension = null;
+
+            foreach (var fileExtension in allowedFileExtensions)
+            {
+                if (System.IO.File.Exists($"{fullPath}{fileExtension}"))
+                {
+                    expectedPathToAvatar = $"{fullPath}{fileExtension}";
+                    expectedFileExtension = fileExtension;
+                    break;
+                }
+            }
+
+            if (expectedPathToAvatar == null)
+            {
+                return BadRequest();
+            }
+
+            var mimeType = "image/jpeg"; 
+
+            return PhysicalFile(expectedPathToAvatar, mimeType, $"{fileName}{expectedFileExtension}");
         }
     }
 }
