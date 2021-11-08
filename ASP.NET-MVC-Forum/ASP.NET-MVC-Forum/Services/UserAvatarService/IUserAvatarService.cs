08a6f07e8236ae74f56a7ab9f2aefe52d3aad451
donchodonev <dummyacc9292@gmail.com>
2021-11-08 16:18:22 +0200

    add user avatar service

---
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Services/UserAvatarService/IUserAvatarService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ASP.NET_MVC_Forum.Services.UserAvatarService
+{
+    public interface IUserAvatarService
+    {
+        /// <summary>
+        /// Gets said image's extension if extension is amongst the allowed image extensions defined at ASP.NET_MVC_Forum.Data.DataConstants.AllowedImageExtensions that are used in the implementation of the GetImageExtension() method in UserAvatarService
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public string GetImageExtension(IFormFile image);
+
+
+        /// <summary>
+        /// Uploads user avatar image asynchronously
+        /// </summary>
+        /// <param name="file">IFormFile - the image</param>
+        /// <param name="width">Desired image width - default is 50px</param>
+        /// <param name="height">Desired image height - default is 50px</param>
+        /// <returns></returns>
+        public Task<string> UploadAvatarAsync(IFormFile file, int width = 50, int height = 50);
+    }
+}
---
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Services/UserAvatarService/UserAvatarService.cs
@@ -0,0 +1,60 @@
+﻿
+namespace ASP.NET_MVC_Forum.Services.UserAvatarService
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using SixLabors.ImageSharp;
+    using SixLabors.ImageSharp.Processing;
+    using System;
+    using System.Threading.Tasks;
+    using static ASP.NET_MVC_Forum.Data.DataConstants.AllowedImageExtensions;
+    using static ASP.NET_MVC_Forum.Data.DataConstants.WebConstants;
+    public class UserAvatarService : IUserAvatarService
+    {
+        private readonly IWebHostEnvironment enviroment;
+
+        public UserAvatarService(IWebHostEnvironment enviroment)
+        {
+            this.enviroment = enviroment;
+        }
+
+        public string GetImageExtension(IFormFile image)
+        {
+            string[] validFileExtensions = new string[5] { JPG, JPEG, PNG, WEBP, BMP };
+
+            string imageExtensionName = null;
+
+            foreach (var currentFileExtension in validFileExtensions)
+            {
+                if (image.FileName.EndsWith(currentFileExtension))
+                {
+                    imageExtensionName = currentFileExtension;
+                    break;
+                }
+            }
+
+            return imageExtensionName;
+        }
+
+        public async Task<string> UploadAvatarAsync(IFormFile file,int width = 50,int height = 50)
+        {
+            string imageExtension = GetImageExtension(file);
+
+            string guid = Guid.NewGuid().ToString();
+
+            var fileName = $"{guid}{imageExtension}";
+
+            string root = enviroment.ContentRootPath;
+
+            string fullPath = $"{root}{AvatarDirectoryPath}{fileName}";
+
+            using (Image image = Image.Load(file.OpenReadStream()))
+            {
+                image.Mutate(x => x.Resize(width,height));
+                await image.SaveAsync(fullPath);
+            }
+
+            return fileName;
+        }
+    }
+}
