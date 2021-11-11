f7c15c95cb825fca713574f75ce119e5f0b95535
donchodonev <dummyacc9292@gmail.com>
2021-11-11 12:05:53 +0200

    fixed custom js code for chart.js bug where chart was working okay only on mobile devices

--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Areas/API/Controllers/StatsController.cs
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Areas/API/Controllers/StatsController.cs
@@ -2,11 +2,19 @@
 
 namespace ASP.NET_MVC_Forum.Areas.API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class StatsController : ControllerBase
     {
         public StatsController()
         {
 
+        }
+
+        [Route("most-commented-posts/{count:int?}")]
+        public ActionResult GetMostCommentedPosts(int resultCount = 5)
+        {
+            return this.LocalRedirect("");
         }
     }
 }
--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/wwwroot/js/chart-js-setup.js
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/wwwroot/js/chart-js-setup.js
@@ -7,39 +7,37 @@ const myChart = new Chart(ctx, {
             label: '# of Comments',
             data: [12, 19, 3, 5, 2, 3, 44],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'red',
+                'green',
+                'blue',
+                'yellow',
+                'purple',
+                'orange',
+                'brown',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'brown',
+                'orange',
+                'purple',
+                'yellow',
+                'blue',
+                'green',
+                'red',
             ],
             borderWidth: 1
         }]
     },
     options: {
+        animation: {
+            onComplete: function () {
+                element = document.getElementById('download-chart-image');
+                element.href = this.toBase64Image();
+                element.download = 'chart.png';
+            }
+        },
         scales: {
             y: {
                 beginAtZero: true
-            }
-
-        },
-         animation: {
-            onComplete: function () {
-                 var image = myChart.toBase64Image();
-                 element = document.getElementById('download-chart-image');
-                 element.href = image;
-                 element.download = 'chart.png';
             }
         }
     }
