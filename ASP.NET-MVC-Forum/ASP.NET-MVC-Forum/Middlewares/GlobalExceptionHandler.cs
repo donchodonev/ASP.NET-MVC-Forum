d4d5338ed8728575776a82454b946b0ad6402abf
Doncho Donev <donevdoncho92@gmail.com>
2022-03-25 09:41:57 +0200

    added global exception handling middleware added AppException class

---
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,51 @@
+﻿using ASP.NET_MVC_Forum.Models;
+
+using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ASP.NET_MVC_Forum.Middlewares
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case AppException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
