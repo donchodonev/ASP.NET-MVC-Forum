d2f34c09275ed362d014e106d7379d5992df4a71
Doncho Donev <donevdoncho92@gmail.com>
2022-04-14 09:07:45 +0300

    setup dependencies to test chart service

--- ASP.NET-MVC-Forum/ASP.NET-MVC-Forum.Business/ChartService.cs
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum.Business/ChartService.cs
@@ -1,10 +1,15 @@
 ﻿namespace ASP.NET_MVC_Forum.Business.Contracts
 {
     using ASP.NET_MVC_Forum.Data.Contracts;
+    using ASP.NET_MVC_Forum.Domain.Entities;
     using ASP.NET_MVC_Forum.Domain.Models.Stats;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
 
     using Microsoft.EntityFrameworkCore;
 
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -13,18 +18,18 @@
 
     public class ChartService : IChartService
     {
-        private readonly IChartRepository chartRepo;
         private readonly IPostRepository postRepo;
         private readonly ICategoryRepository categoryRepo;
+        private readonly IMapper mapper;
 
         public ChartService(
-            IChartRepository chartRepo,
             IPostRepository postRepo,
-            ICategoryRepository categoryRepo)
+            ICategoryRepository categoryRepo,
+            IMapper mapper)
         {
-            this.chartRepo = chartRepo;
             this.postRepo = postRepo;
             this.categoryRepo = categoryRepo;
+            this.mapper = mapper;
         }
 
         public async Task<List<MostCommentedPostsResponeModel>> GetMostCommentedPostsChartDataAsync(int count)
@@ -34,9 +39,7 @@
                 .Where(x => !x.IsDeleted)
                 .Include(x => x.Comments);
 
-            var chartData = await chartRepo
-                .GetStatsAs<MostCommentedPostsResponeModel>(count, posts)
-                .ToListAsync();
+            var chartData = await GetStatsAs<MostCommentedPostsResponeModel>(count, posts).ToListAsync();
 
             AddColor(chartData);
 
@@ -50,9 +53,7 @@
                 .Where(x => !x.IsDeleted)
                 .Include(x => x.Votes);
 
-            var chartData = await chartRepo
-                .GetStatsAs<MostLikedPostsResponeModel>(count, posts)
-                .ToListAsync();
+            var chartData = await GetStatsAs<MostLikedPostsResponeModel>(count, posts).ToListAsync();
 
             AddColor(chartData);
 
@@ -66,9 +67,7 @@
                 .Where(x => !x.IsDeleted)
                 .Include(x => x.Reports);
 
-            var chartData = await chartRepo
-                .GetStatsAs<MostReportedPostsResponeModel>(count, posts)
-                .ToListAsync();
+            var chartData = await GetStatsAs<MostReportedPostsResponeModel>(count, posts).ToListAsync();
 
             AddColor(chartData);
 
@@ -82,8 +81,7 @@
                 .Where(x => !x.IsDeleted)
                 .Include(x => x.Posts);
 
-            var chartData = await chartRepo
-                .GetStatsAs<MostPostsPerCategoryResponseModel>(count, categories)
+            var chartData = await GetStatsAs<MostPostsPerCategoryResponseModel>(count, categories)
                 .ToListAsync();
 
             AddColor(chartData);
@@ -100,6 +98,41 @@
                 ( "Most reported posts", "/api/stats/most-reported-posts"),
                 ( "Most posts by category", "/api/stats/most-posts-by-category")
             };
+        }
+
+        public IQueryable<T> GetStatsAs<T>(int count, IQueryable<Post> posts)
+        {
+            return TakeValidCountOf<Post>(posts, count)
+                    .ProjectTo<T>(mapper.ConfigurationProvider);
+        }
+
+        public IQueryable<T> GetStatsAs<T>(int count, IQueryable<Category> categories)
+        {
+            return TakeValidCountOf<Category>(categories, count)
+                    .ProjectTo<T>(mapper.ConfigurationProvider);
+        }
+
+        private IQueryable<T> TakeValidCountOf<T>(IQueryable<T> items, int requestedCount)
+        {
+            int itemsTotalCount = items.Count();
+
+            int lowestCountBetweenTotalPostCountAndTheCountOfColors =
+                Math.Min(itemsTotalCount,
+                Colors.Length);
+
+            int lowestCountBetweenTotalPostCountAndTheCountOfColorsAndRequestedPostsCount =
+                Math.Min(
+                    lowestCountBetweenTotalPostCountAndTheCountOfColors,
+                    requestedCount);
+
+            if (lowestCountBetweenTotalPostCountAndTheCountOfColorsAndRequestedPostsCount >= requestedCount)
+            {
+                return items
+                        .Take(requestedCount);
+            }
+
+            return items
+                    .Take(lowestCountBetweenTotalPostCountAndTheCountOfColorsAndRequestedPostsCount);
         }
 
         private void AddColor<T>(List<T> posts) where T : IStatsResponseModel
