a7c351136169e85b75268cc509b8aecc242ee808
Doncho Donev <donevdoncho92@gmail.com>
2022-04-17 00:10:13 +0300

    added chat controller tests setup

---
+++ ASP.NET-MVC-Forum/ASP.NET-MVC-Forum.Tests/Controllers/ChatControllerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ASP.NET_MVC_Forum.Tests.Controllers
+{
+    using ASP.NET_MVC_Forum.Business.Contracts;
+    using ASP.NET_MVC_Forum.Web.Controllers;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    public class ChatControllerTests
+    {
+        private ChatController chatController;
+        private Mock<IChatService> chatServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            chatServiceMock = new Mock<IChatService>();
+            chatController = new ChatController(chatServiceMock.Object);
+        }
+    }
+}
